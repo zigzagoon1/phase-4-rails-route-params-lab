d26a6f9e4e9e9bbc0b7b2cd786f735b32b53328f
KellyMark <kmark602@gmail.com>
2023-08-07 17:31:54 -0400

    commit tree 21f095131ce9e17200b81a3a9714fe87d7102e9b

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,16 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    students = []
+    students << Student.find_by(last_name: params[:name].capitalize())
+    students << Student.find_by(first_name: params[:name].capitalize())
+    byebug
     render json: students
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+
+  get '/students/:id', to: 'students#show'
+  get '/students?', to: 'students#index'
   get '/students', to: 'students#index'
   get '/students/grades', to: 'students#grades'
   get '/students/highest-grade', to: 'students#highest_grade'
+
+  
 end
